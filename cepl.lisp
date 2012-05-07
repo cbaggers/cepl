0d786a51c2895e521ada4da87aa39218a5e53fa6
Chris Bagley <chris.bagley@gmail.com>
2012-05-07 14:50:47 +0100

    OK so I am trying to get my head around packages still but I have written/ripped-off-from-ogre a few vector3 functions and will continue adding more. I want to look into the possibility of using something (macros?) to do the donkey work of writing the vector a matrix functions for multiple dimensions. We need it to be hard coded for speed reasons but there's no reason to be maintaining 3 implementations (2, 3 & 4 dimension) when we can just maintain 1.
    My current thinking (again we will see if this bears out) is to
    make these packages for different dimensions and then have a
    'core-math' package that ties it all together with generics.

--- cepl.lisp
+++ cepl.lisp
@@ -2,17 +2,5 @@
 
 (in-package #:cepl)
 
-;;; "cepl" goes here. Hacks and glory await!
-
-;;; Maths bits, not in the right place but I just need
-;;; to start writing.
-
-;;; need a zero, unit-x, unit-y, unit-z, negative of all units
-;;; & unit scale (all 1's)
-
-;;; need to inline
-;;; we will make destructive and no destructive versions,
-;;; looking at some existing code the desctructive versions
-;;; end up being comparitively fast to C. I will use nd as local
-;;; parlance for non-destructive.
-;;; see http://stackoverflow.com/questions/8356494/efficient-vector-operations-of-linear-algebra-in-common-lisp-especially-sbcl for more details
+(defun test ()
+  (print "test func"))
--- package.lisp
+++ package.lisp
@@ -1,5 +1,12 @@
 ;;;; package.lisp
 
+(defpackage #:cepl-vec3
+  (:use #:cl)
+  (:export :vec3 :c-= :c-/= :c-+ :c=+1 :c--1 :c-*
+	   :c-vec* :c-/ :c-vec/ :c-negate :c-length-squared
+	   :c-length :c-distance-squared :c-distance :c-dot
+	   :c-absolute-dot :c-normalize :c-cross))
+
 (defpackage #:cepl
-  (:use #:cl))
+  (:use #:cl #:cepl-vec3))
 
