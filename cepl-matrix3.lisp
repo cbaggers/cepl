8a4cdef21b991e8c9bfbd62003a7023c5bcd6840
Chris Bagley <chris.bagley@gmail.com>
2012-05-08 23:03:07 +0100

    Plenty of changes this time.
    cepl-vec3
    ---------
    I had a good read of the speed chapter in Paul Graham's
    'ANSI Common Lisp' it's been a good help clarifying how
    smart lisp can be about optimising things.
    I have gone back through vec3 and added inline and type
    declarations a plenty.
    I may have over done it a bit with the inlining but that's
    how its done in Ogre so I'm copying that for now.
    Also there were two functions which used the &rest
    parameter which I couldn't work out how to optimise.
    Finally I have replaced the obscure variable names with
    ones that are much clearer
    
    cepl-matrix3
    ------------
    I have also started fleshing out the matrix3 package, this
    is going well but I haven't got how I want it to behave
    ironed out properly in my head yet
    
    package
    -------
    I've had to tweak the exports for vec3 as I changed a couple
    of the function names. I'm still not sure the naming
    convention will remain as it is now but it's early days yet.
    
    Thats all for now!

--- cepl-matrix3.lisp
+++ cepl-matrix3.lisp
@@ -1,7 +1,116 @@
-(in-package :cepl-matrix3)
+(in-package #:cepl-matrix3)
 
-;; All code adapted from Ogre which in turn was adapted 
-;; from Wild Magic 0.2 Matrix math (free source code)
-;; http://www.geometrictools.com/
+;; Code adapted from Ogre which in turn was adapted 
+;; from Wild Magic 0.2 Matrix math (free source code 
+;; http://www.geometrictools.com/) and also from Nklien's
+;; excelent few posts on optimizing common lisp
+;; http://nklein.com/tags/optimization/
 
-;;We assume right handed co-ordinate 
+;; The coordinate system is assumed to be right-handed.
+;; Coordinate axis rotation matrices are of the form
+;;   RX =    1       0       0
+;;           0     cos(t) -sin(t)
+;;           0     sin(t)  cos(t)
+;; where t > 0 indicates a counterclockwise rotation in the yz-plane
+;;   RY =  cos(t)    0     sin(t)
+;;           0       1       0
+;;        -sin(t)    0     cos(t)
+;; where t > 0 indicates a counterclockwise rotation in the zx-plane
+;;   RZ =  cos(t) -sin(t)    0
+;;         sin(t)  cos(t)    0
+;;           0       0       1
+;; where t > 0 indicates a counterclockwise rotation in the xy-plane.
+
+;; remember that matrix co-ords go row then column, unlike how we 
+;; are used to. The axies (as it were) are generally named m & n
+;; where m is rows and n is columns
+
+;; matrices will be stored as 1D arrays with 9 elements, this is for
+;; speed reasons
+
+;; taken from Ogre, I'm assuming this is some kind of lower float 
+;; boundary
+;; const Real Matrix3::ms_fSvdEpsilon = 1e-04;
+
+;; used for single value decomposition...need to real up on this again
+;; const unsigned int Matrix3::ms_iSvdMaxIterations = 32;
+
+(defun make-matrix3 ()
+  (make-array 9 :initial-element 0))
+
+(defun matrix-row (matrix row-num)
+  (assert (and (>= row-num 0) (< row-num 3)))
+  (let ((offset (* row-num 3)))
+    (vector (svref matrix offset)
+	    (svref matrix (+ 1 offset))
+	    (svref matrix (+ 2 offset)))))
+
+(defun matrix-column (matrix col-num)
+  (assert (and (>= col-num 0) (< col-num 3)))
+  (vector (svref matrix col-num)
+	  (svref matrix (+ 3 col-num))
+	  (svref matrix (+ 6 col-num))))
+
+(defun matrix-element (matrix row col)
+  (svref matrix (+ col (* row 3))))
+
+(defun c-identity ()
+  #(1 0 0 0 1 0 0 0 1))
+
+(defun c-= (matrix-a matrix-b)
+  (loop for i 
+     below 9
+     if (/= (svref matrix-a i) (svref matrix-b i))
+     do (return nil)
+     finally (return t)))
+
+(defun c-+ (matrix-a matrix-b)
+  (let ((r (make-matrix3)))
+    (loop for i below 9
+	 do (setf (svref r i) (+ (svref matrix-a i) 
+				 (svref matrix-b i))))
+    r))
+
+(defun c-- (matrix-a matrix-b)
+  (let ((r (make-matrix3)))
+    (loop for i below 9
+	 do (setf (svref r i) (- (svref matrix-a i) 
+				 (svref matrix-b i))))
+    r))
+
+(defun c-* (matrix-a matrix-b)
+  (let ((result (make-matrix3)))
+    (loop for row-num below 3
+	 (loop for col-num below 3
+	      do (let ((row (matrix-row matrix-a))
+		       (col (matrix-column matrix b)))
+		   (setf (svref result (+ col (* row 3)) 
+				(+ ()))))))
+    result))
+
+;; (defun c-* (matrix-a matrix-b)
+;;   (let ((result (make-matrix3)))
+;;     (loop for row below 3
+;; 	 (loop for col below 3
+;; 	      do (setf (svref result)
+;; 		       (+ (matrix-element matrix-a 0 col)))))
+;;     result))
+
+;; 4x4 matrix vec multiply
+(declaim (ftype (function ((simple-array single-float (12))
+                           (simple-array single-float (3)))
+                          (simple-array single-float (3))) mvl*))
+(defun mvl* (matrix vec)
+  (declare (optimize (speed 3) (safety 0)))
+  (let ((ret (make-array 3 :initial-element 0.0f0
+                           :element-type 'single-float)))
+    (loop for jj from 0 below 3
+       do (let ((offset (* jj 4)))
+            (setf (aref ret jj)
+                  (+ (aref matrix (+ offset 3))
+                     (loop for ii from 0 below 3
+                        for kk from offset below (+ offset 3)
+                        summing (* (aref vec ii)
+                                   (aref matrix kk))
+                        single-float)))))
+    ret))
--- cepl-vec3.lisp
+++ cepl-vec3.lisp
@@ -26,143 +26,335 @@
 ;;; need a zero, unit-x, unit-y, unit-z, negative of all units
 ;;; & unit scale (all 1's)
 
-(defun vec3 (&optional (x 0) (y 0) (z 0))
-  (vector x y z))
+;----------------------------------------------------------------
 
+(declaim (inline make-vector3)
+	 (ftype (function ((single-float) 
+			   (single-float) 
+			   (single-float)) 
+			  (simple-array single-float (3))) 
+		make-vector3))
+(defun make-vector3 (x y z)
+  "This takes 3 floats and give back a vector3, this is just an
+   array but it specifies the array type and populates it. 
+   For speed reasons it will not accept integers so make sure 
+   you hand it floats."
+  (declare (single-float x y z))
+  (let (( vec (make-array 3 :element-type `single-float)))
+    (setf (aref vec 0) x
+	  (aref vec 1) y
+	  (aref vec 2) z)
+    vec))
+
+;----------------------------------------------------------------
+
+;; Not sure what I'm going to do with these. I don't belive this
+;; is the best way to do this as it doesnt give a new vector
+;; not that lispy
 (defparameter *unit-x* (vector 1.0 0.0 0.0))
 (defparameter *unit-y* (vector 0.0 1.0 0.0))
 (defparameter *unit-z* (vector 0.0 0.0 1.0))
 (defparameter *unit-scale* (vector 1.0 1.0 1.0))
 
-(defun c-x (u)
-  (svref u 0))
+;----------------------------------------------------------------
 
-(defun c-y (u)
-  (svref u 1))
+;; These have been defined as macros as it want to guarantee they
+;; are 'inlined' as it were and also to avoid any possible cost 
+;; of the implied let. THe 'implied let' thing is an assumption 
+;; on my part from something I read in "ansi common lisp" 
+;; (declaim (inline single?))
+;; (defun single? (lst)
+;;   (and (consp lst) (null (cdr lst))))
+;; (defun foo (x)
+;;   (single? (bar x)))
+;; is equivilent to 
+;; (defun foo (x)
+;;   (let ((lst (bar x)))
+;;     (and (consp lst) (bull (cdr lst)))))
+;; Final justification is that it's purely for syntatic clarity
+;; and not for any computational reason.
+(defmacro c-x (vec)
+  `(aref ,vec 0))
 
-(defun c-z (u)
-  (svref u 2))
+(defmacro c-y (vec)
+  `(aref ,vec 1))
 
-(defun c-= (u v)
-  (and (= (svref u 0) (svref v 0))
-       (= (svref u 1) (svref v 1))
-       (= (svref u 1) (svref v 2))))
+(defmacro c-z (vec)
+  `(aref ,vec 2))
 
-(defun c-/= (u v)
-  (or (/= (svref u 0) (svref v 0))
-      (/= (svref u 1) (svref v 1))
-      (/= (svref u 1) (svref v 2)))) 
+;----------------------------------------------------------------
 
+;; Would be interesting to see if checking that the arrays
+;; are not 'eq' first would speed this up 
+(declaim (inline c-=)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (boolean)) c-=))
+(defun c-= (vector-a vector-b)
+  "Returns either t if the two vectors are equal. 
+   Otherwise it returns nil."
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (and (= (c-x vector-a) (c-x vector-b))
+       (= (c-y vector-a) (c-y vector-b))
+       (= (c-z vector-a) (c-z vector-b))))
+
+;----------------------------------------------------------------
+
+(declaim (inline c-/=)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (boolean)) c-/=))
+(defun c-/= (vector-a vector-b)
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  "Returns either t if the two vectors are not equal. 
+   Otherwise it returns nil."
+  (or (/= (c-x vector-a) (c-x vector-b))
+      (/= (c-y vector-a) (c-y vector-b))
+      (/= (c-z vector-a) (c-z vector-b)))) 
+
+;----------------------------------------------------------------
+
+;; Not sure how to optomise this
 (defun c-+ (&rest vec3s)
   (reduce #'c-+1 vec3s))
 
-(defun c-+1 (u v)
-  (vector (+ (svref u 0) (svref v 0))
-	  (+ (svref u 1) (svref v 1))
-	  (+ (svref u 2) (svref v 2))))
+;----------------------------------------------------------------
 
+(declaim (inline c-+1)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (simple-array single-float (3))) c-+1))
+(defun c-+1 (vector-a vector-b)
+  "Add two vectors and return a new vector containing the result"
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (make-vector3 (+ (c-x vector-a) (c-x vector-b))
+		(+ (c-y vector-a) (c-y vector-b))
+		(+ (c-z vector-a) (c-z vector-b))))
+
+;----------------------------------------------------------------
+
+;; Not sure how to optomise this
 (defun c-- (&rest vec3s)
+  "minus list of vector3s"
   (reduce #'c--1 vec3s))
 
-(defun c--1 (u v)
-  (vector (- (svref u 0) (svref v 0))
-	  (- (svref u 1) (svref v 1))
-	  (- (svref u 2) (svref v 2))))
+;----------------------------------------------------------------
 
-(defun c-* (u a)
-  (vector (* (svref u 0) a)
-	  (* (svref u 1) a)
-	  (* (svref u 2) a)))
+(declaim (inline c--1)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (simple-array single-float (3))) c--1))
+(defun c--1 (vector-a vector-b)
+  "Subtract two vectors and return a new vector containing 
+   the result"
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (make-vector3 (- (c-x vector-a) (c-x vector-b))
+		(- (c-y vector-a) (c-y vector-b))
+		(- (c-z vector-a) (c-z vector-b))))
 
-(defun c-vec* (u v) 
+;----------------------------------------------------------------
+
+(declaim (inline c-*)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (single-float)) 
+			  (simple-array single-float (3))) c-*))
+(defun c-* (vector-a a)
+  "Multiply vector by scalar"
+  (declare ((simple-array single-float (3)) vector-a)
+	   ((single-float) a))
+  (make-vector3 (* (c-x vector-a) a)
+		(* (c-y vector-a) a)
+		(* (c-z vector-a) a)))
+
+;----------------------------------------------------------------
+
+(declaim (inline c-*vec)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (simple-array single-float (3))) 
+		c-*vec))
+(defun c-*vec (vector-a vector-b) 
   "Multiplies components, is not dot product, not sure what
    i'll need this for yet but hey!"
-  (vector (* (svref u 0) (svref v 0))
-	  (* (svref u 1) (svref v 1))
-	  (* (svref u 2) (svref v 2))))
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (make-vector3 (* (c-x vector-a) (c-x vector-b))
+		(* (c-y vector-a) (c-y vector-b))
+		(* (c-z vector-a) (c-z vector-b))))
+
+;----------------------------------------------------------------
 
 ;;; may just be an evil side effect of ripping this off from
 ;;; from ogre but some of the optomisations will be coming over
 ;;; as well, this may not be relevent in lisp, I will see later
 ;;; on.
-(defun c-/ (u a)
+(declaim (inline c-/)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (single-float)) 
+			  (simple-array single-float (3))) c-/))
+(defun c-/ (vector-a a)
+  "divide vector by scalar and return result as new vector"
+  (declare ((simple-array single-float (3)) vector-a)
+	   ((single-float) a))
   (let ((b (/ 1 a)))
-    (vector (* (svref u 0) b)
-	    (* (svref u 1) b)
-	    (* (svref u 2) b))))
+    (make-vector3 (* (c-x vector-a) b)
+		  (* (c-y vector-a) b)
+		  (* (c-z vector-a) b))))
 
-(defun c-vec/ (u v) 
-  "Multiplies components, is not dot product, not sure what
-   i'll need this for yet but hey!"
-  (vector (/ (svref u 0) (svref v 0))
-	  (/ (svref u 1) (svref v 1))
-	  (/ (svref u 2) (svref v 2))))
+;----------------------------------------------------------------
 
-(defun c-negate (u)
-  (vector (- (svref u 0))
-	  (- (svref u 1))
-	  (- (svref u 2))))
+(declaim (inline c-/vec)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (simple-array single-float (3))) 
+		c-/vec))
+(defun c-/vec (vector-a vector-b) 
+  "Divides components, not sure what, i'll need this for 
+   yet but hey!"
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (make-vector3 (/ (c-x vector-a) (c-x vector-b))
+		(/ (c-y vector-a) (c-y vector-b))
+		(/ (c-z vector-a) (c-z vector-b))))
 
-(defun c-length-squared (u)
-  (let ((x (svref u 0))
-	(y (svref u 1))
-	(z (svref u 2)))
+;----------------------------------------------------------------
+
+(declaim (inline c-negate)
+	 (ftype (function ((simple-array single-float (3))) 
+			  (simple-array single-float (3))) 
+		c-negate))
+(defun c-negate (vector-a)
+  "Return a vector that is the negative of the vector passed in"
+  (declare ((simple-array single-float (3)) vector-a))
+  (make-vector3 (- (c-x vector-a))
+		(- (c-y vector-a))
+		(- (c-z vector-a))))
+
+;----------------------------------------------------------------
+
+(declaim (inline c-length-squared)
+	 (ftype (function ((simple-array single-float (3))) 
+			  (single-float)) c-length-squared))
+(defun c-length-squared (vector-a)
+  "Return the squared length of the vector. A regular length
+   is the square root of this value. The sqrt function is slow
+   so if all thats needs doing is to compare lengths then always
+   use the length squared function"
+  (declare ((simple-array single-float (3)) vector-a))
+  (let ((x (c-x vector-a))
+	(y (c-y vector-a))
+	(z (c-z vector-a)))
     (+ (* x x) (* y y) (* z z))))
 
-(defun c-length (u)
+;----------------------------------------------------------------
+
+(declaim (inline c-length)
+	 (ftype (function ((simple-array single-float (3))) 
+			  (single-float)) c-length))
+(defun c-length (vector-a)
   "If you only need to compare relative lengths then definately
-   stick to length-squared as the sqrt is a slow operation.
-   Also c-length-squared should be inlined inside here
-   eventually. "
-  (sqrt (c-length-squared u)))
+   stick to length-squared as the sqrt is a slow operation."
+  (declare ((simple-array single-float (3)) vector-a))
+  (sqrt (c-length-squared vector-a)))
 
-(defun c-distance-squared (u v)
+;----------------------------------------------------------------
+
+(declaim (inline c-distance-squared)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (single-float)) 
+		c-distance-squared))
+(defun c-distance-squared (vector-a vector-b)
   "finds the squared distance between 2 points defined by vectors
-   u & v"
-  (c-length-squared (c-- v u)))
+   vector-a & vector-b"
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (c-length-squared (c-- vector-b vector-a)))
 
-(defun c-distance (u v)
-  (sqrt (c-distance-squared u v)))
+;----------------------------------------------------------------
 
-(defun c-dot (u v)
-  (let ((ux (svref u 0))
-	(uy (svref u 1))
-	(uz (svref u 2))
-	(vx (svref v 0))
-	(vy (svref v 1))
-	(vz (svref v 2)))
+(declaim (inline c-distance)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (single-float)) 
+		c-distance))
+(defun c-distance (vector-a vector-b)
+  "Return the distance between 2 points defined by vectors 
+   vector-a & vector-b. If comparing distances, use 
+   c-distance-squared as it desnt require a sqrt and thus is 
+   faster."
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (sqrt (c-distance-squared vector-a vector-b)))
+
+;----------------------------------------------------------------
+
+(declaim (inline c-dot)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (single-float)) 
+		c-dot))
+(defun c-dot (vector-a vector-b)
+  "Return the dot product of the vector-a and vector-b."
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (let ((ux (c-x vector-a))
+	(uy (c-y vector-a))
+	(uz (c-z vector-a))
+	(vx (c-x vector-b))
+	(vy (c-y vector-b))
+	(vz (c-z vector-b)))
     (+ (* ux vx) (* uy vy) (* uz vz))))
 
-(defun c-absolute-dot (u v) 
-  (+ (abs (* (c-x u) (c-x v))) 
-     (abs (* (c-y u) (c-y v))) 
-     (abs (* (c-z u) (c-z v)))))
+;----------------------------------------------------------------
 
-(defun c-normalize (u)
+(declaim (inline c-absolute-dot)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (single-float)) 
+		c-absolute-dot))
+(defun c-absolute-dot (vector-a vector-b) 
+  "Return the absolute dot product of the vector-a and vector-b."
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (+ (abs (* (c-x vector-a) (c-x vector-b))) 
+     (abs (* (c-y vector-a) (c-y vector-b))) 
+     (abs (* (c-z vector-a) (c-z vector-b)))))
+
+;----------------------------------------------------------------
+
+(declaim (inline c-normalize)
+	 (ftype (function ((simple-array single-float (3))) 
+			  (simple-array single-float (3))) 
+		c-normalize))
+(defun c-normalize (vector-a)
   "This normalizes the vector, it makes sure a zero length
    vector won't throw an error and assumes any length less
    than 1e-08 is 0. THis makes sense with floating point
    inaccuracies."
-  (let ((a (c-length u))) 
+  (declare ((simple-array single-float (3)) vector-a))
+  (let ((a (c-length vector-a))) 
     (if (> a 1e-08) 
-	(c-/ u a)
-	u)))
+	(c-/ vector-a a)
+	vector-a)))
 
-(defun c-cross (u v)
+;----------------------------------------------------------------
+
+(declaim (inline c-cross)
+	 (ftype (function ((simple-array single-float (3)) 
+			   (simple-array single-float (3))) 
+			  (simple-array single-float (3)))
+		c-cross))
+(defun c-cross (vector-a vector-b)
   "Calculates the cross-product of 2 vectors, i.e. the vector 
-that lies perpendicular to them both. The resultign vector
- will <b>NOT</b> be normalised, to maximise efficiency
-The returned vector will be on the side from which the arc 
-from u to v is anticlockwise.
-This is because CEPL uses a right-handed coordinate system."
-  (let ((ux (svref u 0))
-	(uy (svref u 1))
-	(uz (svref u 2))
-	(vx (svref v 0))
-	(vy (svref v 1))
-	(vz (svref v 2)))
-    (vector (- (* uy vz) (* uz vy))
-	    (- (* uz vx) (* ux vz))
-	    (- (* ux vy) (* uy vx)))))
+   that lies perpendicular to them both. The resultign vector
+   will <b>NOT</b> be normalised, to maximise efficiency
+   The returned vector will be on the side from which the arc 
+   from u to v is anticlockwise.
+   This is because CEPL uses a right-handed coordinate system."
+  (declare ((simple-array single-float (3)) vector-a vector-b))
+  (let ((ux (c-x vector-a))
+	(uy (c-y vector-a))
+	(uz (c-z vector-a))
+	(vx (c-x vector-b))
+	(vy (c-y vector-b))
+	(vz (c-z vector-b)))
+    (make-vector3 (- (* uy vz) (* uz vy))
+		  (- (* uz vx) (* ux vz))
+		  (- (* ux vy) (* uy vx)))))
 
 
