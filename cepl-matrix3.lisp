35ee64172db98d7c2ef0c665438a4c71470573a4
Chris Bagley <chris.bagley@gmail.com>
2012-05-08 23:16:17 +0100

    tidied up column widths. Yes they are less than 70 chars, but I like having 3 columns in emacs :)

--- cepl-matrix3.lisp
+++ cepl-matrix3.lisp
@@ -11,72 +11,92 @@
 ;;   RX =    1       0       0
 ;;           0     cos(t) -sin(t)
 ;;           0     sin(t)  cos(t)
-;; where t > 0 indicates a counterclockwise rotation in the yz-plane
+;; where t > 0 indicates a counterclockwise rotation in the 
+;; yz-plane
 ;;   RY =  cos(t)    0     sin(t)
 ;;           0       1       0
 ;;        -sin(t)    0     cos(t)
-;; where t > 0 indicates a counterclockwise rotation in the zx-plane
+;; where t > 0 indicates a counterclockwise rotation in the 
+;; zx-plane
 ;;   RZ =  cos(t) -sin(t)    0
 ;;         sin(t)  cos(t)    0
 ;;           0       0       1
-;; where t > 0 indicates a counterclockwise rotation in the xy-plane.
+;; where t > 0 indicates a counterclockwise rotation in the 
+;; xy-plane.
 
-;; remember that matrix co-ords go row then column, unlike how we 
-;; are used to. The axies (as it were) are generally named m & n
-;; where m is rows and n is columns
+;; remember that matrix co-ords go row then column, unlike 
+;; how we are used to. The axies (as it were) are generally
+;; named m & n where m is rows and n is columns
 
-;; matrices will be stored as 1D arrays with 9 elements, this is for
-;; speed reasons
+;; matrices will be stored as 1D arrays with 9 elements, 
+;; this is for speed reasons
 
-;; taken from Ogre, I'm assuming this is some kind of lower float 
-;; boundary
+;; taken from Ogre, I'm assuming this is some kind of lower 
+;; float boundary
 ;; const Real Matrix3::ms_fSvdEpsilon = 1e-04;
 
-;; used for single value decomposition...need to real up on this again
+;; used for single value decomposition...need to read up on 
+;; this again
 ;; const unsigned int Matrix3::ms_iSvdMaxIterations = 32;
 
 (defun make-matrix3 ()
   (make-array 9 :initial-element 0))
 
+;----------------------------------------------------------------
+
 (defun matrix-row (matrix row-num)
   (assert (and (>= row-num 0) (< row-num 3)))
   (let ((offset (* row-num 3)))
-    (vector (svref matrix offset)
-	    (svref matrix (+ 1 offset))
-	    (svref matrix (+ 2 offset)))))
+    (vector (aref matrix offset)
+	    (aref matrix (+ 1 offset))
+	    (aref matrix (+ 2 offset)))))
+
+;----------------------------------------------------------------
 
 (defun matrix-column (matrix col-num)
   (assert (and (>= col-num 0) (< col-num 3)))
-  (vector (svref matrix col-num)
-	  (svref matrix (+ 3 col-num))
-	  (svref matrix (+ 6 col-num))))
+  (vector (aref matrix col-num)
+	  (aref matrix (+ 3 col-num))
+	  (aref matrix (+ 6 col-num))))
+
+;----------------------------------------------------------------
 
 (defun matrix-element (matrix row col)
-  (svref matrix (+ col (* row 3))))
+  (aref matrix (+ col (* row 3))))
+
+;----------------------------------------------------------------
 
 (defun c-identity ()
   #(1 0 0 0 1 0 0 0 1))
 
+;----------------------------------------------------------------
+
 (defun c-= (matrix-a matrix-b)
   (loop for i 
      below 9
-     if (/= (svref matrix-a i) (svref matrix-b i))
+     if (/= (aref matrix-a i) (aref matrix-b i))
      do (return nil)
      finally (return t)))
+
+;----------------------------------------------------------------
 
 (defun c-+ (matrix-a matrix-b)
   (let ((r (make-matrix3)))
     (loop for i below 9
-	 do (setf (svref r i) (+ (svref matrix-a i) 
-				 (svref matrix-b i))))
+	 do (setf (aref r i) (+ (aref matrix-a i) 
+				 (aref matrix-b i))))
     r))
+
+;----------------------------------------------------------------
 
 (defun c-- (matrix-a matrix-b)
   (let ((r (make-matrix3)))
     (loop for i below 9
-	 do (setf (svref r i) (- (svref matrix-a i) 
-				 (svref matrix-b i))))
+	 do (setf (aref r i) (- (aref matrix-a i) 
+				 (aref matrix-b i))))
     r))
+
+;----------------------------------------------------------------
 
 (defun c-* (matrix-a matrix-b)
   (let ((result (make-matrix3)))
@@ -84,17 +104,11 @@
 	 (loop for col-num below 3
 	      do (let ((row (matrix-row matrix-a))
 		       (col (matrix-column matrix b)))
-		   (setf (svref result (+ col (* row 3)) 
+		   (setf (aref result (+ col (* row 3)) 
 				(+ ()))))))
     result))
 
-;; (defun c-* (matrix-a matrix-b)
-;;   (let ((result (make-matrix3)))
-;;     (loop for row below 3
-;; 	 (loop for col below 3
-;; 	      do (setf (svref result)
-;; 		       (+ (matrix-element matrix-a 0 col)))))
-;;     result))
+;----------------------------------------------------------------
 
 ;; 4x4 matrix vec multiply
 (declaim (ftype (function ((simple-array single-float (12))
