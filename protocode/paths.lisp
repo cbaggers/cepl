d098f983c6742036f097bbce01c51ff84a8e88fa
Chris Bagley <chris.bagley@gmail.com>
2013-02-15 22:16:19 +0000

    Removed redundant files

--- protocode/paths.lisp
+++
@@ -1,4 +0,0 @@
-	(mapcar #'cgl:make-shader `(,(merge-pathnames "1.vert" *examples-path*) 
-				    ,(merge-pathnames "1.frag" *examples-path*))))
-
-  (defvar *examples-path* (merge-pathnames "examples" (asdf:system-source-directory :cepl)))
