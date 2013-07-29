04e45467ccb99c0b6283d9f250fb9c8eadc8fadd
Chris Bagley <chris.bagley@gmail.com>
2013-07-30 00:50:16 +0100

    Still muddling around with texture ideas

--- cepl-gl-textures.lisp
+++ cepl-gl-textures.lisp
@@ -12,14 +12,21 @@
 
 (in-package :cepl-gl)
 
+;; dont export this
 (defun gen-texture ()
   (first (gl:gen-textures 1)))
 
+;; [TODO] mutable or immutable?
+
+;; Textures are objects that store one or more arrays of
+;; data of some dimensionality.
+;; Shaders can reference them with sampler type
 (defclass texture () 
   ((id :initform (gen-texture) :accessor id)
    (type :initform :none :accessor texture-type)))
 
-
+(defclass mutable-texture (texture) ())
+(defclass immutable-texture (texture) ())
 
 (defun bind-texture (texture &optional type)
   (if (or (null type) (eq type (texture-type texture)))
@@ -29,3 +36,36 @@
                  (setf (texture-type texture) type))
           (error "Texture has already been bound")))
   texture)
+
+;;use with safe-exit thingy?
+(defmacro with-bind-texture ((texture &optional type) &body body)
+  (let ((tex (gensym "texture"))
+        (res (gensym "result")))
+    `(let ((,tex ,texture)) 
+       (bind-texture ,tex ,type)
+       (let ((,res (progn ,@body)))
+         (bind-texture 0 (texture-type ,tex))
+         ,res))))
+
+;; is storage a seperate entity?
+
+(defgeneric tex-push (texture data internal-format width 
+                      height pixel-data-format pixel-data-type
+                      &optional level))
+
+(defmethod tex-push ((texture mutable-texture) data 
+                     internal-format width height
+                     pixel-data-format pixel-data-type
+                     &optional (level 0))
+  (let ((tex-type (texture-type texture)))
+    (case tex-type
+      ((:texture-1d :proxy-texture-1d)
+       (gl:tex-image-1d tex-type level internal-format width 0
+                        pixel-data-format pixel-data-type data))
+      ((:texture-2d :proxy-texture-2d)
+       (gl:tex-image-2d tex-type level internal-format width 
+                        height 0 pixel-data-format pixel-data-type data)))))
+
+;; texture unit
+;; (gl:active-texture )
+
