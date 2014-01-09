8157ded15b74f36a8a1eeb48b4910fae89d06d4a
Baggers <techsnuffle@gmail.com>
2014-01-10 00:28:36 +0100

    more time

--- time/conditional-funcs.lisp
+++ time/conditional-funcs.lisp
@@ -48,31 +48,80 @@
                        (c-expired (c) (ignore c) (incf ,step) nil))))
            (,(length forms) (signal-expired) nil))))))
 
-;; ;;--------------------------------------------------------------------
+;;--------------------------------------------------------------------
 
-;; (defparameter *system-time* (make-time-source))
-;; (defun update-system-time () 
-;;   (setf (time-source-value *system-time*) (absolute-system-time)))
+(defclass time-source () ())
 
-;; (defstruct time-source
-;;   (value 0.0 :type short-float)
-;;   (source nil :type (or null time-source))
-;;   (transform nil :type (or null function)))
+(defclass absolute-time-source (time-source)
+  ((value :initform 0 :initarg :value :type integer 
+          :accessor time-value)
+   (last-updated :initform 0 :initarg :last-updated :type integer 
+                 :accessor last-updated)
+   (parent :initform nil :initarg :parent :type (or null time-source)
+           :accessor parent-source)
+   (transform :initform nil :initarg :transform :type (or null function)
+              :accessor transform)))
 
-;; (defun update-time (source)
-;;   (if (time-source-source source)
-;;       (if (time-source-transform source)
-;;           (setf (time-source-value source) 
-;;                 (funcall (time-source-transform source)
-;;                          (time-source-value (time-source-source source))))
-;;           (setf (time-source-value source) 
-;;                 (time-source-value (time-source-source source))))
-;;       (time-source-value source)))
+(defclass relative-time-source (time-source)
+  ((value :initform 0 :initarg :value :type integer :accessor time-value)
+   (last-updated :initform 0 :initarg :last-updated :type integer 
+                 :accessor last-updated)
+   (parent :initarg :parent :type time-source
+           :accessor parent-source)))
 
-;; (defun before (time &optional (time-source (make-)))
-;;   )
+(defparameter *system-time* (make-instance 'absolute-time-source))
 
-;; ;; 'every' is the temporal implementation of steppers, they eat time from a 
-;; ;; relative source and call functions when 'time-buffer is full'
-;; ;; 'every*' is a version that handles multiple things with one source, perfect
-;; ;; for main loops
+(defun update-system-time () 
+  (setf (time-value *system-time*) (absolute-system-time)))
+
+(defmethod update-time ((source absolute-time-source))
+  (let ((parent-time (time-value (parent-source source))))
+    (setf (last-updated source) parent-time)
+    (if (parent-source source)
+        (if (transform source)
+            (setf (time-value source) 
+                  (floor (funcall (transform source) parent-time)))
+            (setf (time-value source) parent-time))
+        (time-value source))))
+
+(defmethod update-time ((source relative-time-source))  
+  (let ((parent-time (time-value (parent-source source))))
+    (setf (time-value source) (- parent-time (last-updated source)))
+    (setf (last-updated source) parent-time))
+  (time-value source))
+
+(defun make-time-source (&key (type :relative) (parent *system-time*)
+                           (transform nil))
+  (case type
+    (:absolute (make-instance 'absolute-time-source :transform transform
+                              :parent parent))
+    (:relative (if transform
+                   (error "transforms cant currently be used on relative time")
+                   (make-instance 'relative-time-source :parent parent)))
+    (t (error "unknown time-source type"))))
+
+(defun before (time &optional (time-source *system-time*)) 
+  (when (< (time-value time-source) time) (time-value time-source)))
+(defun after (time &optional (time-source *system-time*)) 
+  (when (> (time-value time-source) time) (time-value time-source)))
+(defun between (start-time end-time &optional (time-source *system-time*)) 
+  (when (and (>= (time-value time-source) start-time)
+             (<= (time-value time-source) end-time)) 
+    (time-value time-source)))
+
+
+;; 'every' is the temporal implementation of steppers, they eat time from a 
+;; relative source and call functions when 'time-buffer is full'
+;; 'every*' is a version that handles multiple things with one source, perfect
+;; for main loops
+
+(defun from-now (time-offset &optional (time-source *system-time*))
+  (+ time-offset (time-value time-source)))
+
+(loop :until (after 10000 *source*)
+   :do (print "hi"))
+
+
+(let ((deadline (from-now (seconds 10))))
+  (loop :until (after deadline *source*)
+     :do (print "hi")))
