177e4b8cce2fd489207603eb1e30f66e3179f0fb
Baggers <techsnuffle@gmail.com>
2014-03-25 01:36:41 +0100

    getting closer with time FINALLY :)

--- time/more-time-experiments.lisp
+++ time/more-time-experiments.lisp
@@ -14,42 +14,6 @@
 
 ;;----------------------------------------------------------------------
 
-;; (tlambda ()
-;;     (repeat ((before (from-now 300))
-;;              (setf (pos *e*) (lerp (pos *e*) (v! 1 2 3))))
-;;             ((before (from-now 400))
-;;              (setf (pos *e*) (lerp (pos *e*) (v! 0 0 0)))))
-;;   (before (from-now (seconds 4)) (print "first 4 seconds"))
-;;   (print "hi"))
-
-;; (let ((~c-0 0)
-;;       (~dl-1 (from-now (seconds 4)))
-;;       (~dl-2 (from-now 300))
-;;       ~dl-3)
-;;   (lambda ()
-;;     (case ~c-0
-;;       (0 (if (beforep (~dl-2))
-;;              (progn (setf (pos *e*) (lerp (pos *e*) (v! 1 2 3))))
-;;              (setf ~c-0 1 ~dl-3 (from-now 400))))
-;;       (1 (if (beforep (~dl-3))
-;;              (progn (setf (pos *e*) (lerp (pos *e*) (v! 0 0 0))))
-;;              (setf ~c-0 0 ~dl-2 (from-now 300)))))
-;;     (when (beforep (~dl-1) (print "first 4 seconds")))
-;;     (print "hi")))
-
-;; (body test expired-test inner-let closed-vars)
-
-(tlambda+ (x y) (+ x y))
-
-;;{TODO} should "every time" stil print after it expires?
-(tlambda+ ()
-  ((before (from-now 100)) (print "moo"))
-  (print "every time"))
-
-(tlambda+ () 
-  ((and (each (seconds 1)) (before (from-now (seconds 4)))) 
-   (print "hi")))
-
 ;;{TODO} I think ther are ways to have conflicting expired, address this
 ;;       I was slightly off but had the right idea...only this macro must
 ;;       signal expiry, the other must idicate it so they can be nested
@@ -97,17 +61,11 @@
                                 (multiple-value-list 
                                  (%compile-time-syntax+ (first form))))))))))
 
-;; 1. body - 
-;; 2. test - 
-;; 3. expired-test - 
-;; 4. inner-let - 
-;; 5. closed-vars - proper let form
-
-;;{TODO} the expired is wrong, must hand it down
 (defun tprogn+ (forms)
   (let* ((processed-forms (%process-tprogn-forms forms))
-         (expire-forms (remove nil (mapcar #'third processed-forms))))
-    (list `(let (,(remove nil (mapcan #'fourth processed-forms)))
+         (expire-forms (remove nil (mapcar #'third processed-forms)))
+         (let-forms (remove nil (mapcan #'fourth processed-forms))))
+    (list `(let ,(when let-forms (list let-forms))
              ,@(loop :for (body test expired-test inner-let closed-vars) 
                   :in processed-forms :collect
                   (if (and (eq test t) (null expired-test))
@@ -121,8 +79,9 @@
 (defun tthen+ (forms)
   (let* ((processed-forms (%process-tprogn-forms forms))
          (counter-sym (gensym "counter"))
-         (%closed-vars (remove nil (mapcan #'fifth (copy-tree processed-forms)))))
-    (list `(let (,(remove nil (mapcar #'fourth processed-forms)))
+         (%closed-vars (remove nil (mapcan #'fifth (copy-tree processed-forms))))
+         (let-forms (remove nil (mapcar #'fourth processed-forms))))
+    (list `(let ,(when let-forms (list let-forms))
              (case ,counter-sym
                ,@(loop :for (body test expired-test inner-let closed-vars)
                     :in processed-forms
@@ -134,7 +93,8 @@
                                  `(when ,expired-test
                                     (incf ,counter-sym)
                                     ,@(when (< index (1- (length forms)))
-                                            (loop :for i :in (fifth (nth (+ index 1) processed-forms)) :collect `(setf ,@i))))))))))
+                                            (loop :for i :in (fifth (nth (+ index 1) processed-forms)) :collect `(setf ,@i)))
+                                    nil)))))))
           `(< ,counter-sym ,(length forms))
           `(= ,counter-sym ,(length forms))
           nil
@@ -142,35 +102,12 @@
                 (cons (first %closed-vars) 
                       (mapcar #'listify (rest %closed-vars)))))))
 
-;;{TODO}
-;; ---------------------
-;; EXPAND THE CODE BELOW
-;; ---------------------
-;; Find out how to make sure nested closed-vars are spliced in correctly,
-;; be careful with mapcan. It seems to be related to %closed-vars in trepeat+
-
-(tlambda+ ()
-  (repeat ((before (from-now 100)) (print "--1"))
-        ((before (from-now 200)) (print "--2"))
-        ((before (from-now 300)) (print "--3"))
-        ((before (from-now 400)) (print "--4"))))
-
-(tlambda+ ()
-  (repeat (then ((before (from-now (seconds 2))) (print "1"))
-                ((before (from-now (seconds 3))) (print "2")))
-          ((before (from-now 100)) (print "moo"))
-          ((before (from-now 999)) (print "wwwwww"))
-          ((before (from-now 200)) (print "woo"))))
-
-(tlambda+ ()
-  (repeat (then ((before (from-now (seconds 2))) (print "1")))
-          ((before (from-now 100)) (print "moo"))))
-
 (defun trepeat+ (forms) 
   (let* ((processed-forms (%process-tprogn-forms forms))
          (counter-sym (gensym "counter"))
-         (%closed-vars (remove nil (mapcan #'fifth (copy-tree processed-forms)))))
-    (list `(let (,(remove nil (mapcar #'fourth (copy-list processed-forms))))
+         (%closed-vars (remove nil (mapcan #'fifth (copy-tree processed-forms))))
+         (let-forms (remove nil (mapcar #'fourth (copy-list processed-forms)))))
+    (list `(let ,(when let-forms (list let-forms))
              (case ,counter-sym
                ,@(loop :for (body test expired-test inner-let closed-vars)
                     :in processed-forms
@@ -184,10 +121,48 @@
                                           (cons `(setf ,counter-sym 0)
                                                 (loop :for i :in (fifth (first processed-forms)) :collect `(setf ,@i)))
                                           (cons `(incf ,counter-sym)
-                                                (loop :for i :in (fifth (nth (+ index 1) processed-forms)) :collect `(setf ,@i)))))))))))
+                                                (loop :for i :in (fifth (nth (+ index 1) processed-forms)) :collect `(setf ,@i))))
+                                    nil)))))))
           t
           nil
           nil
           (cons `(,counter-sym 0)
                 (cons (first %closed-vars)
                       (mapcar #'listify (rest %closed-vars)))))))
+
+;; 1. body - 
+;; 2. test - 
+;; 3. expired-test - 
+;; 4. inner-let - 
+;; 5. closed-vars - proper let form
+
+
+;; (tlambda+ (x y) (+ x y))
+
+;;{TODO} should "every time" stil print after it expires?
+;; (tlambda+ ()
+;;   ((before (from-now 100)) (print "moo"))
+;;   (print "every time"))
+
+;; (tlambda+ () 
+;;   ((and (each (seconds 1)) (before (from-now (seconds 4)))) 
+;;    (print "hi")))
+
+;; (tlambda+ ()
+;;   (repeat ((before (from-now 100)) (print "--1"))
+;;           ((before (from-now 200)) (print "--2"))
+;;           ((before (from-now 300)) (print "--3"))
+;;           ((before (from-now 400)) (print "--4"))))
+
+;; (tlambda+ ()
+;;   (repeat (then ((before (from-now (seconds 2))) (print "1"))
+;;                 ((before (from-now (seconds 3))) (print "2")))
+;;           ((before (from-now 100)) (print "moo"))
+;;           ((before (from-now 999)) (print "wwwwww"))
+;;           ((before (from-now 200)) (print "woo"))))
+
+;; (tlambda+ ()
+;;   (repeat (then ((before (from-now (seconds 2))) (print "1")))
+;;           ((before (from-now 100)) (print "moo"))))
+
+
