056018ad0a28188e1faf160bda9c4afb8520bb45
Baggers <chris.bagley@gmail.com>
2015-03-05 16:14:56 +0100

    ideas to come back too

---
+++ protocode/palettes.lisp
@@ -0,0 +1,6 @@
+
+;; Have a big database of palettes, query by id, color, tags, etc
+
+;; have multiple pallete databases, query all at once as standard.
+
+;; have a default pallete database in cepl.
