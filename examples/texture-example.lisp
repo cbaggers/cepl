0118e454374625f9c65a0d1a77360616f813b961
Baggers <chris.bagley@gmail.com>
2015-04-14 00:42:44 +0200

    fix texture example

--- examples/texture-example.lisp
+++ examples/texture-example.lisp
@@ -1,34 +1,37 @@
 ;; Texturing and fragment effects
+(in-package :cepl)
 
 (defparameter *count* 0.0)
 (defparameter *texture* nil)
-(defparameter *vert-gpu* nil)
 (defparameter *v-stream* nil)
 
-(defstruct-g vert-data ()
-  (position :vec4 :accessor pos)
-  (tex-pos :vec2 :accessor tex-pos))
+(defun-g tex-vert ((vert g-pt))
+  (values (v! (pos vert) 1)
+          (:smooth (tex vert))))
 
-(defpipeline ripple-with-wobble ((vert vert-data) &uniform (tex :sampler-2d)
-                                 (count :float) (pos-offset :vec4))
-  (:vertex (setf gl-position (pos vert))
-           (out (tex-coord :smooth) (tex-pos vert)))
-  (:fragment
-   (let* ((rip-size 0.02) (centre (v! 0.0 0.0)) (damp 0.6)
-          (peaks 9.0)
-          (dif (- tex-coord centre))
-          (dist (dot dif dif))
-          (height (/ (+ (sin (+ count (* dist peaks)))
-                        (sin (- count (* (y tex-coord) peaks))))
-                     2.0))
-          (rip-offset (* (* (normalize dif) rip-size) height damp)))
-     (out outputColor (+ (texture tex (+ rip-offset tex-coord))
-                         (v! (* -0.2 height) (* -0.2 height) 0.0 0.0))))))
+(defun-g tex-frag ((tex-coord :vec2) &uniform (texture :sampler-2d)
+                   (count :float) (pos-offset :vec4))
+  (let* ((rip-size 0.02) (centre (v! 0.0 0.0)) (damp 0.6)
+         (peaks 9.0)
+         (dif (- tex-coord centre))
+         (dist (dot dif dif))
+         (height (/ (+ (sin (+ count (* dist peaks)))
+                       (sin (- count (* (y tex-coord) peaks))))
+                    2.0))
+         (rip-offset (* (* (normalize dif) rip-size) height damp)))
+    (+ (texture texture (+ rip-offset tex-coord))
+       (v! (* -0.2 height) (* -0.2 height) 0.0 0.0))))
+
+(defpipeline ripple-with-wobble () (g-> #'tex-vert #'tex-frag))
 
 (defun step-demo ()
-  (ripple-with-wobble *v-stream* :tex *texture* :count *count*
-                      :pos-offset (v! 0 0 0 0))
-  (incf *count* 0.01))
+  (evt.sdl:pump-events)
+  (update-swank)
+  (cgl:clear :color-buffer-bit)
+  (gmap #'ripple-with-wobble *v-stream*
+        :texture *texture* :count *count* :pos-offset (v! 0 0 0 0))
+  (incf *count* 0.08)
+  (cgl:update-display))
 
 (let ((running nil))
   (defun run-demo ()
@@ -37,22 +40,17 @@
     (apply #'gl:viewport 0 0 cgl:+default-resolution+)
     (let* ((img-data (loop :for i :below 64 :collect
                         (loop :for j :below 64 :collect (random 254)))))
-      (setf *vert-gpu*
-            (make-gpu-array `((,(v! -0.5 -0.366 0.0 1.0) ,(v! -1.0 1.0))
-                              (,(v!  0.5 -0.366 0.0 1.0) ,(v!  1.0 1.0))
-                              (,(v!  0.0    0.5 0.0 1.0) ,(v!  0.0 -1.0)))
-                            :dimensions 3 :element-type 'vert-data))
-      (setf *v-stream* (make-buffer-stream *vert-gpu*))
+      (setf *v-stream*
+            (make-buffer-stream
+             (make-gpu-array `((,(v! -0.5 -0.366 0) ,(v! -1  1))
+                               (,(v!  0.5 -0.366 0) ,(v!  1  1))
+                               (,(v!    0    0.5 0) ,(v!  0 -1)))
+                             :dimensions 3 :element-type 'g-pt)
+             :retain-arrays t))
       (setf *texture* (with-c-array
                           (temp (make-c-array img-data :dimensions '(64 64)
                                               :element-type :ubyte))
                         (make-texture temp)))
-      (loop :while running :do
-         (case-events (event) (:quit () (setf running nil)))
-         (update-swank)
-         (base-macros:continuable
-           (cgl:clear :color-buffer-bit)
-           (step-demo)
-           (cgl:flush)
-           (cgl:update-display)))))
+      (loop :while running :do (continuable (step-demo)))))
   (defun stop-demo () (setf running nil)))
+(evt:observe (|sys|) (when (typep e 'evt.sdl:will-quit) (stop-demo)))
