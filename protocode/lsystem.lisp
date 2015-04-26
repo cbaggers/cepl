79c0c5bc9f6e0b9a68c94e8eff716b498c29cd02
Baggers <chris.bagley@gmail.com>
2015-04-26 10:27:48 +0200

    protocode stuff

--- protocode/lsystem.lisp
+++ protocode/lsystem.lisp
@@ -1,16 +1,18 @@
+(in-package :cepl)
+(in-readtable fn_:fn_lambda)
+
+
 ;; recurse n times
 (defun rec-n (func n d)
   (if (> n 0)
       (rec-n func (- n 1) (funcall func d))
       d))
 
-
-
 ;; lsystem
 (defun l (x lang) (mapcat λ(cdr (assoc _ lang)) x))
 
 ;; one lsystem language
-(setf l1 '((a a b) (b c c) (c a b)))
+(defparameter l1 '((a a b) (b c c) (c a b)))
 
 ;; 6 recursions
 (rec-n λ(l _ l1) 6 '(a))
