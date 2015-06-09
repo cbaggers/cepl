e5561dc7318ef255f8b89a0954d75e716114fec9
Baggers <chris.bagley@gmail.com>
2015-06-09 09:52:55 +0200

    better directory search for osx

--- platform-specific/osx.lisp
+++ platform-specific/osx.lisp
@@ -1,9 +1,13 @@
 (in-package :cepl-osx)
 
 (defun start ()
-  (let ((port-dir (cl-fad:directory-exists-p "/opt/local/lib/")))
-    (when (and port-dir
-               (not (member port-dir cffi:*foreign-library-directories*)))
-      (push port-dir cffi:*foreign-library-directories*)
-      (sdl2:make-this-thread-main
-       (lambda () (swank:create-server :style nil :dont-close t))))))
+  (let ((extra-package-dirs '("/opt/local/lib/" "/usr/local/lib")))
+    (mapcar
+     (lambda (raw-path)
+       (let ((port-dir (cl-fad:directory-exists-p "/opt/local/lib/")))
+         (when (and port-dir
+                    (not (member port-dir cffi:*foreign-library-directories*)))
+           (push port-dir cffi:*foreign-library-directories*))))
+     extra-package-dirs)
+    (sdl2:make-this-thread-main
+     (lambda () (swank:create-server :style nil :dont-close t)))))
