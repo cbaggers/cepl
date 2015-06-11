655a92124d1c6f9b6fd969c40d05e6d7471b2dbb
Baggers <chris.bagley@gmail.com>
2015-06-11 11:34:39 +0200

    search more places for libs on osx This should cover the brew installed libs as well as macports

--- platform-specific/osx.lisp
+++ platform-specific/osx.lisp
@@ -4,7 +4,7 @@
   (let ((extra-package-dirs '("/opt/local/lib/" "/usr/local/lib")))
     (mapcar
      (lambda (raw-path)
-       (let ((port-dir (cl-fad:directory-exists-p "/opt/local/lib/")))
+       (let ((port-dir (cl-fad:directory-exists-p raw-path)))
          (when (and port-dir
                     (not (member port-dir cffi:*foreign-library-directories*)))
            (push port-dir cffi:*foreign-library-directories*))))
