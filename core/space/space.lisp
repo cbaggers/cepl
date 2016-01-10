203e624814492f8f32b909d66305bc27669fdc7a
Baggers <baggers@sett.lan>
2016-01-11 00:05:36 +0100

    I understand how spaces will work now :D
    Finally got the last piece of the puzzle in my head so now I see how
    this feature will look and the language used to talk about it.
    
    I now need to implement a dumb version so I can test stuff. After that
    we will need something that actually performs well, but holy crap I'm so
    relieved!
    
    `protocode/space/the-breakdown.txt` is the place to see the overall
    picture. I will be filling out details there.

--- core/space/space.lisp
+++ core/space/space.lisp
@@ -1,6 +1,7 @@
 (in-package :space)
 
 ;;----------------------------------------------------------------------
+;; Space Events
 
 (defstruct (space-event (:include cpl-event))
   (flush nil
@@ -8,14 +9,14 @@
   (matrix-4 (m4:identity-matrix4)
             :type (simple-array single-float (16))))
 
-(defun %uid (space) (evt::event-node-uid space))
-
 ;;----------------------------------------------------------------------
+;; Space Node
 
 (evt::def-event-node-type space
   (transform (m4:identity-matrix4)
              :type (simple-array single-float (16)))
-  (has-propagated nil :type boolean))
+  (has-propagated nil :type boolean)
+  (nht nil :type hash-table)) ;; {TODO} swap out later
 
 (defmethod print-object ((object space) stream)
   (format stream "#<SPACE ~s>" (%uid object)))
@@ -25,7 +26,10 @@
   (assert (or (null parent-space) (typep parent-space 'space)))
   (make-space :transform transform :subscribe-to parent-space))
 
+(defun %uid (space) (evt::event-node-uid space))
+
 ;;----------------------------------------------------------------------
+;; Hierarchical Transforms
 
 (defun parent-space (space)
   (first (evt::event-node-subscriptions space)))
@@ -34,7 +38,34 @@
   (m4:affine-inverse (space-transform space)))
 
 ;;----------------------------------------------------------------------
-;; gpu
+;; Non-Hierarchical Transforms
+
+(defstruct transformer
+  to
+  from)
+
+(defun add-non-hierarchical-relationship (space-a space-b a->b b->a)
+  (assert (and (null (parent-space space-a))
+	       (null (parent-space space-b))))
+  (assert (no-space-cycles space-a space-b))
+  (setf (gethash space-b (space-nht space-a))
+	(make-transformer :to a->b :from b->a)
+	(gethash space-a (space-nht space-b))
+	(make-transformer :to b->a :from a->b))
+  space-a)
+
+(defun remove-non-hierarchical-relationship (space-a space-b)
+  (assert (and (null (parent-space space-a))
+	       (null (parent-space space-b))))
+  (remhash space-b (space-nht space-a))
+  (remhash space-a (space-nht space-b)))
+
+(defun no-space-cycles (space-a space-b)
+  (declare (ignore space-a space-b))
+  t)
+
+;;----------------------------------------------------------------------
+;; GPU
 
 (varjo::def-v-type-class space-g (varjo::v-type)
   ((varjo::core :initform nil :reader varjo::core-typep)
