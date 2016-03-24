2b4fa1af9fa415d4f93440415d77c61db5baa64f
baggers <baggers@cave>
2016-03-24 22:46:27 +0100

    Docs symbs now from package externed, not original

--- docs/api/gen.lisp
+++ docs/api/gen.lisp
@@ -16,8 +16,7 @@
 
 (defun gen-docs ()
   (staple:generate
-   :cepl :packages '(:cepl
-		     :cepl.c-arrays
+   :cepl :packages '(:cepl.c-arrays
 		     :cepl.gpu-arrays.buffer-backed
 		     :cepl.streams
 		     :cepl.viewports
@@ -34,7 +33,6 @@
 		     :cepl.render-state
 		     :cepl.space
 		     :cepl.types
-
 		     :cepl.context)
    :template *template-dir*
    :out *out-dir*
@@ -45,3 +43,31 @@
    :out *host-out-dir*
    :if-exists :supersede)
   :booya)
+
+(org.tymoonnext.staple::define-tag-processor do-external-symbols (node)
+  (let ((package (plump:attribute node "package"))
+        (sort (or (plump:attribute node "sort") "#'symb-type<"))
+        (exclude (cl-ppcre:split "\\s+" (plump:attribute node "exclude"))))
+    (plump:remove-attribute node "package")
+    (plump:remove-attribute node "sort")
+    (plump:remove-attribute node "exclude")
+    (org.tymoonnext.staple::process-attributes node)
+    (let ((package (org.tymoonnext.staple::resolve-value (read-from-string package))))
+      (org.tymoonnext.staple::process-attribute
+       node "iterate"
+       (sort
+        (remove-if #'(lambda (symb) (org.tymoonnext.staple::%is-excluded symb exclude))
+                   (package-symbol-objects2 package))
+        (org.tymoonnext.staple::resolve-value (read-from-string sort)))))))
+
+(defun package-symbol-objects2 (package)
+  "Gathers all possible symbol-objects of the given package."
+  (org.tymoonnext.staple:symbol-objects (package-symbols2 package)))
+
+(defun package-symbols2 (package)
+  "Gets all symbols within a package."
+  (let ((lst ())
+        (package (find-package package)))
+    (do-external-symbols (s package)
+      (push s lst))
+    lst))
