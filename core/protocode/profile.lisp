51c2c289a4d5eb58d8b869ff2f40a1ecca67594c
Baggers <chris.bagley@gmail.com>
2017-05-23 01:57:42 +0200

    profile-everything macro

---
+++ core/protocode/profile.lisp
@@ -0,0 +1,75 @@
+(defmacro profile-all-cepl ()
+  (let ((packages '(:cepl-utils
+                    :cepl.errors
+                    :cepl.host
+                    :cepl.lifecycle
+                    :cepl.measurements
+                    :%cepl.types
+                    :cepl.memory
+                    :cepl.types.foreign
+                    :cepl.types
+                    :cepl.types.predefined
+                    :cepl.internals
+                    :cepl.render-state
+                    :cepl.context
+                    :cepl.viewports
+                    :cepl.image-formats
+                    :cepl.pixel-formats
+                    :cepl.c-arrays
+                    :cepl.gpu-buffers
+                    :cepl.gpu-arrays.buffer-backed
+                    :cepl.vaos
+                    :cepl.streams
+                    :cepl.ubos
+                    :cepl.textures
+                    :cepl.gpu-arrays.texture-backed
+                    :cepl.gpu-arrays
+                    :cepl.samplers
+                    :cepl.fbos
+                    :cepl.blending
+                    :cepl.pipelines
+                    :cepl.misc)))
+    `(sb-profile:profile
+      ,@(loop :for p :in packages :append
+           (let ((pkg (find-package p)))
+             (loop :for s :being :the symbol :in p
+                :when (and (eq pkg (symbol-package s)) (fboundp s))
+                :collect s))))))
+
+(defmacro unprofile-all-cepl ()
+  (let ((packages '(:cepl-utils
+                    :cepl.errors
+                    :cepl.host
+                    :cepl.lifecycle
+                    :cepl.measurements
+                    :%cepl.types
+                    :cepl.memory
+                    :cepl.types.foreign
+                    :cepl.types
+                    :cepl.types.predefined
+                    :cepl.internals
+                    :cepl.render-state
+                    :cepl.context
+                    :cepl.viewports
+                    :cepl.image-formats
+                    :cepl.pixel-formats
+                    :cepl.c-arrays
+                    :cepl.gpu-buffers
+                    :cepl.gpu-arrays.buffer-backed
+                    :cepl.vaos
+                    :cepl.streams
+                    :cepl.ubos
+                    :cepl.textures
+                    :cepl.gpu-arrays.texture-backed
+                    :cepl.gpu-arrays
+                    :cepl.samplers
+                    :cepl.fbos
+                    :cepl.blending
+                    :cepl.pipelines
+                    :cepl.misc)))
+    `(sb-profile:unprofile
+      ,@(loop :for p :in packages :append
+           (let ((pkg (find-package p)))
+             (loop :for s :being :the symbol :in p
+                :when (and (eq pkg (symbol-package s)) (fboundp s))
+                :collect s))))))
