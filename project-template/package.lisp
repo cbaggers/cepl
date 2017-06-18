2b2108899738027b6a5129209215b3c1cbdc2e36
Baggers <chris.bagley@gmail.com>
2017-06-19 00:22:56 +0200

    use the newer cepl.skitter package

--- project-template/package.lisp
+++ project-template/package.lisp
@@ -2,5 +2,5 @@
 
 (uiop:define-package (#| TMPL_VAR name |#)
     (:use #:cl #:cepl #:rtg-math
-          (#| TMPL_IF skitter-sdl-p |#):skitter.sdl2.keys :skitter.sdl2.mouse-buttons(#| /TMPL_IF |#)
+          (#| TMPL_IF skitter-sdl-p |#):cepl.skitter(#| /TMPL_IF |#)
           (#| TMPL_IF livesupport-p |#):livesupport(#| /TMPL_IF |#)))
