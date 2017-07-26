935c053fb2c06ed2ddda1c01c9c2f653ab6b65dd
cbaggers <chris.bagley@gmail.com>
2017-07-26 17:33:53 +0200

    define free for sampler

--- core/samplers/samplers.lisp
+++ core/samplers/samplers.lisp
@@ -247,6 +247,9 @@
     (warn "CEPL: free-sampler not yet implemented~%leaking ~s"
           sampler)))
 
+(defmethod free ((sampler sampler))
+  (free-sampler sampler))
+
 (defmacro with-temp-sampler ((var tex) &body body)
   (assert (and (symbolp var) (not (keywordp var))))
   `(let ((,var (sample ,tex)))
