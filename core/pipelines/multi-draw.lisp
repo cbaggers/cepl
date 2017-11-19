b6b8de56b21a4fd84a780314d8392f4c77c1f307
cbaggers <chris.bagley@gmail.com>
2017-11-19 22:44:55 +0100

    and scuppered already due to type stupidity in defstruct-g
    gonna go look at replacing the autowrap hacks..it's about time

---
+++ core/pipelines/multi-draw.lisp
@@ -0,0 +1,14 @@
+(in-package :cepl.pipelines)
+
+;; typedef  struct {
+;;    GLuint  count;
+;;    GLuint  instanceCount;
+;;    GLuint  first;
+;;    GLuint  baseInstance;
+;; } DrawArraysIndirectCommand;
+
+;; (defstruct-g draw-arrays-indirect-command
+;;   (count :uint :accessor daic-count)
+;;   (instance-count :uint :accessor daic-instance-count)
+;;   (first :uint :accessor daic-first)
+;;   (base-instance :uint :accessor daic-base-instance))
