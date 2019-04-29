a85a9c735b0165d540f9956e1fa7fc96a534c4fa
cbaggers <chris.bagley@gmail.com>
2019-04-29 09:01:03 +0200

    refactor the restore code so it's more approachable
    Before it was one monolithic block and was rather intimidating to
    read, now it's a bit more piecemeal which I hope means I flee from it
    less often

--- core/context/known-state-blocks.lisp
+++ core/context/known-state-blocks.lisp
@@ -2,6 +2,9 @@
 
 ;;----------------------------------------------------------------------
 
+;; (with-context-state-restored (:program t)
+;;   foo)
+
 (defmacro with-context-state-restored ((&key
                                         program
                                         stencil
@@ -54,184 +57,233 @@
     (assert (not (eq (first ssbo-indices) 'quote)))
     (let ((ctx (gensym "ctx")))
       `(with-cepl-context (,ctx)
+         (runtime-restore-block-checks ,ctx)
          (unwind-protect (progn ,@body)
-           (restore-state ,ctx
-                          ,program
-                          ,stencil
-                          ,vao
-                          ,fbos-bound
-                          ,depth-test-function
-                          ,depth-mask
-                          ,depth-range
-                          ,depth-clamp
-                          ,clear-color
-                          ,cull-face
-                          ,front-face
-                          ,viewport
-                          ',color-mask-indices
-                          ',tex-unit-ids
-                          ',buffer-targets
-                          ',scissor-viewport-indices
-                          ',ubo-indices
-                          ',ssbo-indices
-                          ,unpack-alignment
-                          ,pack-alignment))))))
+           ,@(when program
+               `((restore-program ,ctx)))
+           ,@(when stencil
+               `((restore-stencil ,ctx)))
+           ,@(when vao
+               `((restore-vao ,ctx)))
+           ,@(when fbos-bound
+               `((restore-fbo-bindings ,ctx)))
+           ,@(when depth-test-function
+               `((restore-depth-test-function ,ctx)))
+           ,@(when depth-mask
+               `((restore-depth-mask ,ctx)))
+           ,@(when depth-range
+               `((restore-depth-range ,ctx)))
+           ,@(when depth-clamp
+               `((restore-depth-clamp ,ctx)))
+           ,@(when cull-face
+               `((restore-cull-face ,ctx)))
+           ,@(when front-face
+               `((restore-front-face ,ctx)))
+           ,@(when clear-color
+               `((restore-clear-color ,ctx)))
+           ,@(when viewport
+               `((restore-viewport ,ctx)))
+           ,@(when unpack-alignment
+               `((restore-unpack-alignment ,ctx)))
+           ,@(when pack-alignment
+               `((restore-pack-alignment ,ctx)))
+           ,@(when color-mask-indices
+               `((restore-color-masks ,ctx)))
+           ,@(when scissor-viewport-indices
+               `((restore-current-scissor-viewports ,ctx)))
+           ,@(when tex-unit-ids
+               `((restore-array-of-bound-samplers ,ctx)))
+           ,@(when buffer-targets
+               `((restore-array-of-bound-gpu-buffers ,ctx)))
+           ,@(when ubo-indices
+               `((restore-array-of-ubo-bindings-buffer-ids ,ctx)))
+           ,@(when ssbo-indices
+               `((restore-array-of-ssbo-bindings-buffer-ids ,ctx))))))))
 
-(defn restore-state ((context cepl-context)
-                     (program boolean)
-                     (stencil boolean)
-                     (vao boolean)
-                     (fbos-bound boolean)
-                     (depth-test-function boolean)
-                     (depth-mask boolean)
-                     (depth-range boolean)
-                     (depth-clamp boolean)
-                     (clear-color boolean)
-                     (cull-face boolean)
-                     (front-face boolean)
-                     (viewport boolean)
-                     (color-mask-indices list)
-                     (tex-unit-ids list)
-                     (buffer-targets list)
-                     (scissor-viewport-indices list)
-                     (ubo-indices list)
-                     (ssbo-indices list)
-                     (unpack-alignment boolean)
-                     (pack-alignment boolean))
-    null
+;;----------------------------------------------------------------------
+
+(defn runtime-restore-block-checks ((context cepl-context)) (values)
+  ;; I'd like to handle the stuff below but Im not sure how yet
+  ;;
+  ;; I dont think these 2 can be done:
+  ;; - array-of-transform-feedback-bindings-buffer-ids
+  ;; - array-of-bound-queries. If these are rebound then they are broken
   (%with-cepl-context-slots (current-tfs
-                             current-viewport
-                             current-blend-params
-                             array-of-bound-gpu-buffers
-                             array-of-bound-samplers
-                             array-of-ubo-binding-ranges
-                             array-of-ubo-bindings-buffer-ids
-                             array-of-ssbo-binding-ranges
-                             array-of-ssbo-bindings-buffer-ids
-                             current-scissor-viewports)
+                             current-blend-params)
       context
-    ;; I'd like to handle this ↓↓ but Im not sure how to handle it yet
     (assert (not current-blend-params) ()
             'state-restore-limitation-blending)
     (assert (not current-tfs) ()
-            'state-restore-limitation-transform-feedback)
+            'state-restore-limitation-transform-feedback))
+  (values))
 
-    ;; I dont think this can be done:
-    ;; - array-of-transform-feedback-bindings-buffer-ids
-    ;; - array-of-bound-queries. If these are rebound then they are broken
+(defn-inline restore-program ((context cepl-context)) (values)
+  (cepl.pipelines::force-use-program context 0)
+  (values))
 
-    ;; current-program
-    (when program
-      (cepl.pipelines::force-use-program context 0))
+(defn restore-stencil ((context cepl-context)) (values)
+  (setf (cepl.stencil:current-stencil-params :front context)
+        (cepl.stencil:current-stencil-params :front context))
 
-    (when stencil
-      (setf (cepl.stencil:current-stencil-params :front context)
-            (cepl.stencil:current-stencil-params :front context))
+  (setf (cepl.stencil:current-stencil-params :back context)
+        (cepl.stencil:current-stencil-params :back context))
 
-      (setf (cepl.stencil:current-stencil-params :back context)
-            (cepl.stencil:current-stencil-params :back context))
+  (multiple-value-bind (front back)
+      (cepl:stencil-mask :front-and-back context)
+    (setf (cepl:stencil-mask :front context) front
+          (cepl:stencil-mask :back context) back))
+  (values))
 
-      (multiple-value-bind (front back)
-          (cepl:stencil-mask :front-and-back context)
-        (setf (cepl:stencil-mask :front context) front
-              (cepl:stencil-mask :back context) back)))
+;; vao-binding-id
+(defn-inline restore-vao ((context cepl-context)) (values)
+  (force-bind-vao (vao-bound context) context)
+  (values))
 
-    ;; vao-binding-id
-    (when vao
-      (force-bind-vao (vao-bound context) context))
+;; fbo-bindings
+(defn-inline restore-fbo-bindings ((context cepl-context)) (values)
+  (%set-read-fbo-no-check context (read-fbo-bound context))
+  (%set-draw-fbo-no-check context (draw-fbo-bound context))
+  (values))
 
-    ;; fbo-bindings
-    (when fbos-bound
-      (%set-read-fbo-no-check context (read-fbo-bound context))
-      (%set-draw-fbo-no-check context (draw-fbo-bound context)))
+;; depth-func
+(defn-inline restore-depth-test-function ((context cepl-context)) (values)
+  (force-depth-test-function (depth-test-function context) context)
+  (values))
 
-    ;; depth-func
-    (when depth-test-function
-      (force-depth-test-function (depth-test-function context) context))
+;; depth-mask
+(defn-inline restore-depth-mask ((context cepl-context)) (values)
+  (setf (depth-mask context) (depth-mask context))
+  (values))
 
-    ;; depth-mask
-    (when depth-mask
-      (setf (depth-mask context) (depth-mask context)))
+;; depth-range
+(defn-inline restore-depth-range ((context cepl-context)) (values)
+  (setf (depth-range-vec2 context) (depth-range-vec2 context))
+  (values))
 
-    ;; depth-range
-    (when depth-range
-      (setf (depth-range-vec2 context) (depth-range-vec2 context)))
+;; depth-clamp
+(defn-inline restore-depth-clamp ((context cepl-context)) (values)
+  (setf (depth-clamp context) (depth-clamp context))
+  (values))
 
-    ;; depth-clamp
-    (when depth-clamp
-      (setf (depth-clamp context) (depth-clamp context)))
+;; cull-face
+(defn-inline restore-cull-face ((context cepl-context)) (values)
+  (setf (cull-face context) (cull-face context))
+  (values))
 
-    ;; cull-face
-    (when cull-face
-      (setf (cull-face context) (cull-face context)))
+;; front-face
+(defn-inline restore-front-face ((context cepl-context)) (values)
+  (setf (front-face context) (front-face context))
+  (values))
 
-    ;; front-face
-    (when front-face
-      (setf (front-face context) (front-face context)))
+;; clear-color
+(defn-inline restore-clear-color ((context cepl-context)) (values)
+  (setf (clear-color context) (clear-color context))
+  (values))
 
-    ;; clear-color
-    (when clear-color
-      (setf (clear-color context) (clear-color context)))
+;; viewport
+(defn-inline restore-viewport ((context cepl-context)) (values)
+  (%with-cepl-context-slots (current-viewport)
+      context
+    (cepl.viewports::%set-current-viewport context current-viewport)
+    (values)))
 
-    ;; viewport
-    (when viewport
-      (cepl.viewports::%set-current-viewport context current-viewport))
+(defn-inline restore-unpack-alignment ((context cepl-context)) (values)
+  (setf (unpack-alignment t context) (unpack-alignment context))
+  (values))
 
-    (when unpack-alignment
-      (setf (unpack-alignment t context) (unpack-alignment context)))
+(defn-inline restore-pack-alignment ((context cepl-context)) (values)
+  (setf (pack-alignment t context) (pack-alignment context))
+  (values))
 
-    (when pack-alignment
-      (setf (pack-alignment t context) (pack-alignment context)))
+(defn restore-color-masks ((context cepl-context)
+                           (color-mask-indices list))
+    (values)
+  (loop
+     :for index :in color-mask-indices
+     :for mask := (color-mask index)
+     :when mask
+     :do (setf (color-mask index context) mask))
+  (values))
 
-    ;; color-masks
-    (loop :for index :in color-mask-indices
-       :for mask := (color-mask index)
-       :when mask
-       :do (setf (color-mask index context) mask))
-
-    ;; current-scissor-viewports
-    (loop :for index :in scissor-viewport-indices
+(defn restore-current-scissor-viewports ((context cepl-context)
+                                         (scissor-viewport-indices list))
+    (values)
+  (%with-cepl-context-slots (current-scissor-viewports)
+      context
+    (loop
+       :for index :in scissor-viewport-indices
        :for viewport := (aref current-scissor-viewports index)
        :when viewport
-       :do (cepl.scissor::force-scissor-viewport viewport index context))
+       :do (cepl.scissor::force-scissor-viewport viewport index context)))
+  (values))
 
-    ;; array-of-bound-samplers
-    (loop :for id :in tex-unit-ids
+(defn restore-array-of-bound-samplers ((context cepl-context)
+                                       (tex-unit-ids list))
+    (values)
+  (%with-cepl-context-slots (array-of-bound-samplers)
+      context
+    (loop
+       :for id :in tex-unit-ids
        :for sampler := (aref array-of-bound-samplers id)
        :when sampler
-       :do (force-sampler-bound context sampler id))
+       :do (force-sampler-bound context sampler id)))
+  (values))
 
-    ;; array-of-bound-gpu-buffers
-    (loop :for target :in buffer-targets
+(defn restore-array-of-bound-gpu-buffers ((context cepl-context)
+                                          (buffer-targets list))
+    (values)
+  (%with-cepl-context-slots (array-of-bound-gpu-buffers)
+      context
+    (loop
+       :for target :in buffer-targets
        :for index := (buffer-kind->cache-index target)
        :for buffer := (aref array-of-bound-gpu-buffers index)
        :when buffer
-       :do (setf (gpu-buffer-bound context target) buffer))
+       :do (setf (gpu-buffer-bound context target) buffer)))
+  (values))
 
-    ;; array-of-ubo-bindings-buffer-ids
-    (loop :for ubo-binding-point :in ubo-indices
-       :for ubo-id := (aref array-of-ubo-bindings-buffer-ids ubo-binding-point)
+(defn restore-array-of-ubo-bindings-buffer-ids ((context cepl-context)
+                                                (ubo-indices list))
+    (values)
+  (%with-cepl-context-slots (array-of-ubo-binding-ranges
+                             array-of-ubo-bindings-buffer-ids)
+      context
+    (loop
+       :for ubo-binding-point :in ubo-indices
+       :for ubo-id := (aref array-of-ubo-bindings-buffer-ids
+                            ubo-binding-point)
        :do
-       (unless (unknown-gl-id-p ubo-id)
-         (let* ((range-index (the array-index (* ubo-id 2)))
-                (offset (aref array-of-ubo-binding-ranges range-index))
-                (size (aref array-of-ubo-binding-ranges (+ range-index 1))))
-           (%gl:bind-buffer-range
-            #.(gl-enum :uniform-buffer) ubo-binding-point ubo-id offset size))))
+         (unless (unknown-gl-id-p ubo-id)
+           (let* ((range-index (the array-index (* ubo-id 2)))
+                  (offset (aref array-of-ubo-binding-ranges range-index))
+                  (size (aref array-of-ubo-binding-ranges
+                              (+ range-index 1))))
+             (%gl:bind-buffer-range #.(gl-enum :uniform-buffer)
+                                    ubo-binding-point
+                                    ubo-id
+                                    offset
+                                    size)))))
+  (values))
 
-    ;; array-of-ssbo-bindings-buffer-ids
-    (loop :for ssbo-binding-point :in ssbo-indices
-       :for ssbo-id := (aref array-of-ssbo-bindings-buffer-ids ssbo-binding-point)
+(defn restore-array-of-ssbo-bindings-buffer-ids ((context cepl-context)
+                                                 (ssbo-indices list))
+    (values)
+  (%with-cepl-context-slots (array-of-ssbo-binding-ranges
+                             array-of-ssbo-bindings-buffer-ids)
+      context
+    (loop
+       :for ssbo-binding-point :in ssbo-indices
+       :for ssbo-id := (aref array-of-ssbo-bindings-buffer-ids
+                             ssbo-binding-point)
        :do
-       (unless (unknown-gl-id-p ssbo-id)
-         (let* ((range-index (the array-index (* ssbo-id 2)))
-                (offset (aref array-of-ssbo-binding-ranges range-index))
-                (size (aref array-of-ssbo-binding-ranges (+ range-index 1))))
-           (%gl:bind-buffer-range
-            #.(gl-enum :uniform-buffer) ssbo-binding-point ssbo-id offset size))))
-
-    ;; ensure blending changes havent leaked
-    (cepl.blending::set-current-blend-params nil)
-    nil))
-
-;;----------------------------------------------------------------------
+         (unless (unknown-gl-id-p ssbo-id)
+           (let* ((range-index (the array-index (* ssbo-id 2)))
+                  (offset (aref array-of-ssbo-binding-ranges range-index))
+                  (size (aref array-of-ssbo-binding-ranges
+                              (+ range-index 1))))
+             (%gl:bind-buffer-range #.(gl-enum :uniform-buffer)
+                                    ssbo-binding-point
+                                    ssbo-id
+                                    offset
+                                    size)))))
+  (values))
