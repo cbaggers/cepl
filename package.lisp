7bacc32d71edbb785fdee30e7bf4b65941fb3995
cbaggers <chris.bagley@gmail.com>
2019-10-10 14:22:18 +0200

    export c-array-byte-size from cepl.c-arrays

--- package.lisp
+++ package.lisp
@@ -539,6 +539,7 @@
            :c-array-pointer
            :c-array-dimensions
            :c-array-total-size
+           :c-array-byte-size
            :c-array-element-type
            :c-array-element-byte-size
            :c-array-free
